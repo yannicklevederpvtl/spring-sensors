be2f13510b0055e800bd49e8ad9d89dbe44da1d0
dprocha <dprocha85@gmail.com>
2024-07-31 10:05:49 -0300

    Changed label Spring

--- src/main/java/org/tanzu/demo/DemoController.java
+++ src/main/java/org/tanzu/demo/DemoController.java
@@ -52,7 +52,7 @@ public class DemoController {
         return new HashMap<>();
     }
 
-    private static final String REPLACEMENT_BANNER_TEXT="Spring Boot Sensors";
+    private static final String REPLACEMENT_BANNER_TEXT="Spring Sensors";
 
     @RequestMapping("/refresh")
     public @ResponseBody SensorData refresh() {
